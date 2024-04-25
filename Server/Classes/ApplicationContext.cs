dad1d3d2bc0e5bdce0b1ab72a91a69436a81ddef
Ivan Zhidkov <ivanzhidkov2005@outlook.com>
2024-04-26 02:25:34 +0500

    ServerCLASSESDBHZ

---
+++ Server/Classes/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QualificationTest
+{
+    class ApplicationContext : DbContext
+    {
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Question> Questions{ get; set; }
+
+        public ApplicationContext() : base("DefaultConnection") { }
+
+    }
+}
---
+++ Server/Classes/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QualificationTest
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string EmployeeName  { get; set; }
+        public string EmployeeLogin { get; set; }
+        public string EmployeePassword { get; set; }
+
+
+        public Employee() { }
+
+        public Employee(int employeeID, string employeeName, string employeeLogin, string employeePassword)
+        {
+            EmployeeID = employeeID;
+            EmployeeName = employeeName;
+            EmployeeLogin = employeeLogin;
+            EmployeePassword = employeePassword;
+        }
+    }
+}
+
