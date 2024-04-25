b60b2dc4a04222b2a8e8bb9c631768db4490035a
Ivan Zhidkov <ivanzhidkov2005@outlook.com>
2024-04-26 03:19:05 +0500

    Working(somehow)

--- QualificationTest/Pages/Authorization.xaml.cs
+++ QualificationTest/Pages/Authorization.xaml.cs
@@ -1,17 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net.WebSockets;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace QualificationTest
 {
@@ -26,8 +19,61 @@ namespace QualificationTest
             InitializeComponent();
         }
 
-        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        private async void LoginButton_Click(object sender, RoutedEventArgs e)
         {
+            using (ClientWebSocket client = new ClientWebSocket())
+            {
+                Uri serviceUri = new Uri("ws://localhost:5000/send");
+                var cTs = new CancellationTokenSource();
+                cTs.CancelAfter(TimeSpan.FromSeconds(120));
+                try
+                {
+                    await client.ConnectAsync(serviceUri, cTs.Token);
+                    while (client.State == WebSocketState.Open)
+                    {
+                        string login = loginTB.Text.ToString();
+                        string password = passwordTB.Password.ToString();
+                        if(!string.IsNullOrEmpty(login) && !string.IsNullOrEmpty(password))
+                        {
+                            ArraySegment<byte> byteToSend = new ArraySegment<byte>(Encoding.UTF8.GetBytes(login));
+                            await client.SendAsync(byteToSend, WebSocketMessageType.Text, true, cTs.Token);
+                            var responseBuffer = new byte[1024];
+                            var offset = 0;
+                            var packet = 1024;
+                            while (true)
+                            {
+                                ArraySegment<byte> byteReceived = new ArraySegment<byte>(responseBuffer, offset, packet);
+                                WebSocketReceiveResult response = await client.ReceiveAsync(byteReceived, cTs.Token);
+                                var responseMessage = Encoding.UTF8.GetString(responseBuffer, offset, response.Count);
+                                clientTB.Text = responseMessage.ToString();
+                                if (response.EndOfMessage)
+                                    break;
+                            }
+
+
+                            ArraySegment<byte> byteToSend1 = new ArraySegment<byte>(Encoding.UTF8.GetBytes(password));
+                            await client.SendAsync(byteToSend1, WebSocketMessageType.Text, true, cTs.Token);
+                            var responseBuffer1 = new byte[1024];
+                            var offset1 = 0;
+                            var packet1 = 1024;
+                            while (true)
+                            {
+                                ArraySegment<byte> byteReceived1 = new ArraySegment<byte>(responseBuffer1, offset1, packet1);
+                                WebSocketReceiveResult response1 = await client.ReceiveAsync(byteReceived1, cTs.Token);
+                                var responseMessage1 = Encoding.UTF8.GetString(responseBuffer1, offset1, response1.Count);
+                                clientTB.Text = responseMessage1.ToString();
+                                if (response1.EndOfMessage)
+                                    break;
+                            }
+                        }
+                    }
+                }
+                catch (WebSocketException ex)
+                {
+                    clientTB.Text = ex.Message.ToString();
+
+                }
+            }
             if (loginConfirmed)
             {
                 NavigationService.Navigate(new MainProgram());
--- Server/Classes/ApplicationContext.cs
+++ Server/Classes/ApplicationContext.cs
@@ -1,19 +1,19 @@
-﻿using System.Data.Entity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System.Data.Entity;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace QualificationTest
-{
-    class ApplicationContext : DbContext
-    {
+//namespace QualificationTest
+//{
+//    class ApplicationContext : DbContext
+//    {
 
-        public DbSet<Employee> Employees { get; set; }
-        public DbSet<Question> Questions{ get; set; }
+//        public DbSet<Employee> Employees { get; set; }
+//        public DbSet<Question> Questions{ get; set; }
 
-        public ApplicationContext() : base("DefaultConnection") { }
+//        public ApplicationContext() : base("DefaultConnection") { }
 
-    }
-}
+//    }
+//}
