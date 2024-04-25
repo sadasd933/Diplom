b6c6eca6dade2185b6ccab7cb1d845f64eb0b244
pmc <pmc@DESKTOP-0FTCSVR>
2024-04-26 02:14:50 +0500

    Добавьте файлы проекта.

---
+++ QualificationTest/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using QualificationTest;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QualificationTest
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFrame.Content = new Authorization();
+        }
+
+
+    }
+}
+
+
+
+//string currentQuestionText;
+//string currentAnswer1;
+//string currentAnswer2;
+//string currentAnswer3;
+//string currentAnswerCorrect;
+//int correctAnswersCount = 0;
+//QuestionDb data;
+
+//public MainWindow()
+//{
+//    InitializeComponent();
+//    data = new QuestionDb();
+//    LoadNewQuestion();
+//}
+
+//void CheckAnswer(string ch)
+//{
+
+//    if (ch == currentAnswerCorrect)
+//    {
+//        correctAnswersCount++;
+//    }
+
+//    LoadNewQuestion();
+//}
+//int a = 0;
+//void LoadNewQuestion()
+//{
+//    if (a < 5)
+//    {
+//        a++;
+
+//        var q = data.CurrentQuestion;
+//        currentAnswer1 = q.Answer1;
+//        currentAnswer2 = q.Answer2;
+//        currentAnswer3 = q.Answer3;
+//        currentQuestionText = q.QuestionText;
+//        currentAnswerCorrect = q.AnswerCorrect;
+
+//        QuestionTextTextBlock.Text = currentQuestionText.ToString();
+//        QuestionAnswer1.Text = currentAnswer1.ToString();
+//        QuestionAnswer2.Text = currentAnswer2.ToString();
+//        QuestionAnswer3.Text = currentAnswer3.ToString();
+//        Progress.Text = a.ToString() + "/5";
+//    }
+//    else
+//    {
+//        QuestionTextTextBlock.Text = "GZ\n\nВы набрали: " + correctAnswersCount.ToString() + " из 5 правильных ответов!";
+//        QuestionAnswer1.Text = string.Empty;
+//        QuestionAnswer2.Text = string.Empty;
+//        QuestionAnswer3.Text = string.Empty;
+//        Answer_TextBox.Visibility = Visibility.Hidden;
+//        SubmitAnswer.Visibility = Visibility.Hidden;
+//    }
+
+
+
+//}
+
+//private void SubmitAnswer_Click(object sender, RoutedEventArgs e)
+//{
+//    CheckAnswer(Answer_TextBox.Text.ToString());
+//}
+//    }
+//*
---
+++ QualificationTest/Pages/Authorization.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QualificationTest
+{
+    /// <summary>
+    /// Логика взаимодействия для Authorization.xaml
+    /// </summary>
+    public partial class Authorization : Page
+    {
+        bool loginConfirmed = false;
+        public Authorization()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (loginConfirmed)
+            {
+                NavigationService.Navigate(new MainProgram());
+            }
+        }
+    }
+}
